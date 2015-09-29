85871448f142f23ed4f59af01ac6ca4a30f65a74
ming6842 <ming6842@hotmail.com>
2015-09-29 23:49:15 +0800

    Minimum code already, crashed

---
+++ stm32f429_Junyou_min/main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+int main(void)
+{
+  
+
+
+  int32_t testx = 777;
+  float aaa=123.1415968f ;
+
+  testx ++;
+  aaa+=0.01f;
+  testx= (int32_t)(aaa * 1.0f);
+   while(1){
+
+  }
+
+
+}
+
